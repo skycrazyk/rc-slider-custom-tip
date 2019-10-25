88f0861e769e854f141e92d75a7e08b1a7f6cf93
Dmitry Karyakin <skycrazyk@gmail.com>
2019-10-25 19:46:55 +0500

    class component fersion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { Component, useRef, createRef } from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Range, Handle } from 'rc-slider';
 import Tooltip from 'rc-tooltip';
@@ -6,265 +6,277 @@ import 'rc-slider/assets/index.css';
 import 'rc-tooltip/assets/bootstrap.css';
 import './style';
 
-const space = 10;
-const tooltipIdMin = 'tooltipIdMin'; // генерировать guid
-const tooltipIdMax = 'tooltipIdMax'; // генерировать guid
-let tooltipMinEl;
-let tooltipMaxEl;
-let rangeEl;
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-// Получаем доступ к DOM элементам
-const onPopupAlign = () => {
-  if (!tooltipMinEl && !tooltipMaxEl) {
-    tooltipMinEl = document.getElementById(tooltipIdMin);
-    tooltipMaxEl = document.getElementById(tooltipIdMax);
+    this.state = {
+      tooltipMinEl: null,
+      tooltipMaxEl: null,
+    };
+
+    this.rangeEl = createRef();
+    this.space = 10;
+    this.tooltipIdMin = 'tooltipIdMin'; // генерировать guid
+    this.tooltipIdMax = 'tooltipIdMax'; // генерировать guid
+    this.customHandle = this.customHandle.bind(this);
+    this.onPopupAlign = this.onPopupAlign.bind(this);
   }
-};
 
-const CustomHandle = props => {
-  const { value, dragging, index, ...restProps } = props;
-  const isMin = index === 0;
+  customHandle(props) {
+    const { tooltipMinEl, tooltipMaxEl } = this.state;
+    const { value, dragging, index, ...restProps } = props;
+    const isMin = index === 0;
 
-  if (rangeEl && rangeEl.current) {
-    const rangeBounds = rangeEl.current.sliderRef.getBoundingClientRect();
+    if (this.rangeEl && this.rangeEl.current) {
+      const rangeBounds = this.rangeEl.current.sliderRef.getBoundingClientRect();
 
-    // Относительно левого края
-    if (isMin && tooltipMinEl) {
-      const tooltipMinElBounds = tooltipMinEl.getBoundingClientRect();
+      // Относительно левого края
+      if (isMin && tooltipMinEl) {
+        const tooltipMinElBounds = tooltipMinEl.getBoundingClientRect();
 
-      const tooltipShowValueEl = tooltipMinEl.querySelector(
-        '.rc-slider-tooltip-show-value'
-      );
+        const tooltipShowValueEl = tooltipMinEl.querySelector(
+          '.rc-slider-tooltip-show-value'
+        );
 
-      const tooltipHideValueEl = tooltipMinEl.querySelector(
-        '.rc-slider-tooltip-hide-value'
-      );
+        const tooltipHideValueEl = tooltipMinEl.querySelector(
+          '.rc-slider-tooltip-hide-value'
+        );
 
-      const tooltipHideValueElBounds = tooltipHideValueEl.getBoundingClientRect();
+        const tooltipHideValueElBounds = tooltipHideValueEl.getBoundingClientRect();
 
-      if (rangeBounds.left >= tooltipMinElBounds.left) {
-        tooltipShowValueEl.style = [
-          'position: fixed',
-          `top: ${tooltipHideValueElBounds.top}px`,
-          `left: ${rangeBounds.left}px`,
-        ].join(';');
-      } else {
-        tooltipShowValueEl.style = [
-          `position: absolute`,
-          'top: 0',
-          'left: 0',
-        ].join(';');
-      }
-    }
-
-    // Относительно правого края
-    if (!isMin && tooltipMaxEl) {
-      const tooltipMaxElBounds = tooltipMaxEl.getBoundingClientRect();
-
-      const tooltipShowValueEl = tooltipMaxEl.querySelector(
-        '.rc-slider-tooltip-show-value'
-      );
-
-      const tooltipHideValueEl = tooltipMaxEl.querySelector(
-        '.rc-slider-tooltip-hide-value'
-      );
-
-      const tooltipHideValueElBounds = tooltipHideValueEl.getBoundingClientRect();
-
-      if (rangeBounds.right <= tooltipMaxElBounds.right) {
-        tooltipShowValueEl.style = [
-          'position: fixed',
-          `top: ${tooltipHideValueElBounds.top}px`,
-          `left: ${rangeBounds.right - tooltipHideValueElBounds.width}px`,
-        ].join(';');
-      } else {
-        tooltipShowValueEl.style = [
-          `position: absolute`,
-          'top: 0',
-          'right: 0',
-        ].join(';');
-      }
-    }
-
-    // Между собой
-    if (tooltipMinEl && tooltipMaxEl) {
-      // Левый бегунок
-      const tooltipMinElBounds = tooltipMinEl.getBoundingClientRect();
-
-      // Левый бегунок - скрытое значение
-      const tooltipMinHideValueEl = tooltipMinEl.querySelector(
-        '.rc-slider-tooltip-hide-value'
-      );
-
-      const tooltipMinHideValueElBounds = tooltipMinHideValueEl.getBoundingClientRect();
-
-      // Левый бегунок - видимое значение
-      const tooltipMinShowValueEl = tooltipMinEl.querySelector(
-        '.rc-slider-tooltip-show-value'
-      );
-
-      const tooltipMinShowValueElBounds = tooltipMinShowValueEl.getBoundingClientRect();
-
-      // Правый бегунок
-      const tooltipMaxElBounds = tooltipMaxEl.getBoundingClientRect();
-
-      // Правый бегунок - скрытое значение
-      const tooltipMaxHideValueEl = tooltipMaxEl.querySelector(
-        '.rc-slider-tooltip-hide-value'
-      );
-
-      const tooltipMaxHideValueElBounds = tooltipMaxHideValueEl.getBoundingClientRect();
-
-      // Правый бегунок - видимое значение
-      const tooltipMaxShowValueEl = tooltipMaxEl.querySelector(
-        '.rc-slider-tooltip-show-value'
-      );
-
-      const tooltipMaxShowValueElBounds = tooltipMaxShowValueEl.getBoundingClientRect();
-
-      // Логика позиционирования
-      if (
-        tooltipMinShowValueElBounds.right + space >=
-        tooltipMaxShowValueElBounds.left
-      ) {
-        // У левого края
         if (rangeBounds.left >= tooltipMinElBounds.left) {
-          tooltipMaxShowValueEl.style = [
+          tooltipShowValueEl.style = [
             'position: fixed',
-            `top: ${tooltipMaxHideValueElBounds.top}px`,
-            `left: ${tooltipMinShowValueElBounds.right + space}px`,
+            `top: ${tooltipHideValueElBounds.top}px`,
+            `left: ${rangeBounds.left}px`,
+          ].join(';');
+        } else {
+          tooltipShowValueEl.style = [
+            `position: absolute`,
+            'top: 0',
+            'left: 0',
           ].join(';');
         }
+      }
 
-        // У правого края
-        else if (rangeBounds.right <= tooltipMaxElBounds.right) {
-          tooltipMinShowValueEl.style = [
+      // Относительно правого края
+      if (!isMin && tooltipMaxEl) {
+        const tooltipMaxElBounds = tooltipMaxEl.getBoundingClientRect();
+
+        const tooltipShowValueEl = tooltipMaxEl.querySelector(
+          '.rc-slider-tooltip-show-value'
+        );
+
+        const tooltipHideValueEl = tooltipMaxEl.querySelector(
+          '.rc-slider-tooltip-hide-value'
+        );
+
+        const tooltipHideValueElBounds = tooltipHideValueEl.getBoundingClientRect();
+
+        if (rangeBounds.right <= tooltipMaxElBounds.right) {
+          tooltipShowValueEl.style = [
             'position: fixed',
-            `top: ${tooltipMinHideValueElBounds.top}px`,
-            `left: ${tooltipMaxShowValueElBounds.left -
-              tooltipMinShowValueElBounds.width -
-              space}px`,
+            `top: ${tooltipHideValueElBounds.top}px`,
+            `left: ${rangeBounds.right - tooltipHideValueElBounds.width}px`,
+          ].join(';');
+        } else {
+          tooltipShowValueEl.style = [
+            `position: absolute`,
+            'top: 0',
+            'right: 0',
           ].join(';');
         }
+      }
 
-        // В центре
-        else {
-          const diff =
-            tooltipMinHideValueElBounds.right +
-            space -
-            tooltipMaxHideValueElBounds.left;
+      // Между собой
+      if (tooltipMinEl && tooltipMaxEl) {
+        // Левый бегунок
+        const tooltipMinElBounds = tooltipMinEl.getBoundingClientRect();
 
-          const half = diff / 2;
+        // Левый бегунок - скрытое значение
+        const tooltipMinHideValueEl = tooltipMinEl.querySelector(
+          '.rc-slider-tooltip-hide-value'
+        );
 
-          const minFixed =
-            tooltipMinHideValueElBounds.left - half < rangeBounds.left;
+        const tooltipMinHideValueElBounds = tooltipMinHideValueEl.getBoundingClientRect();
 
-          const maxFixed =
-            tooltipMaxHideValueElBounds.right + half > rangeBounds.right;
+        // Левый бегунок - видимое значение
+        const tooltipMinShowValueEl = tooltipMinEl.querySelector(
+          '.rc-slider-tooltip-show-value'
+        );
 
-          console.log(maxFixed);
+        const tooltipMinShowValueElBounds = tooltipMinShowValueEl.getBoundingClientRect();
 
-          let tooltipMinLeft = tooltipMinHideValueElBounds.left - half;
-          let tooltipMaxLeft = tooltipMaxHideValueElBounds.left + half;
+        // Правый бегунок
+        const tooltipMaxElBounds = tooltipMaxEl.getBoundingClientRect();
 
-          // Ближе к левому краю
-          if (minFixed) {
-            // позиция левого бегунка
-            tooltipMinLeft = Math.max(
-              tooltipMinHideValueElBounds.left - half,
-              rangeBounds.left
-            );
+        // Правый бегунок - скрытое значение
+        const tooltipMaxHideValueEl = tooltipMaxEl.querySelector(
+          '.rc-slider-tooltip-hide-value'
+        );
 
-            // позиция правого бегунка
-            tooltipMaxLeft = Math.max(
-              tooltipMaxHideValueElBounds.left + half,
-              tooltipMinShowValueElBounds.right + space
-            );
+        const tooltipMaxHideValueElBounds = tooltipMaxHideValueEl.getBoundingClientRect();
+
+        // Правый бегунок - видимое значение
+        const tooltipMaxShowValueEl = tooltipMaxEl.querySelector(
+          '.rc-slider-tooltip-show-value'
+        );
+
+        const tooltipMaxShowValueElBounds = tooltipMaxShowValueEl.getBoundingClientRect();
+
+        // Логика позиционирования
+        if (
+          tooltipMinShowValueElBounds.right + this.space >=
+          tooltipMaxShowValueElBounds.left
+        ) {
+          // У левого края
+          if (rangeBounds.left >= tooltipMinElBounds.left) {
+            tooltipMaxShowValueEl.style = [
+              'position: fixed',
+              `top: ${tooltipMaxHideValueElBounds.top}px`,
+              `left: ${tooltipMinShowValueElBounds.right + this.space}px`,
+            ].join(';');
           }
 
-          // Ближе к правому краю
-          if (maxFixed) {
-            // позиция левого бегунка
-            tooltipMinLeft = Math.min(
-              tooltipMinHideValueElBounds.left - half,
-              rangeBounds.right -
-                tooltipMinHideValueElBounds.width -
-                tooltipMaxHideValueElBounds.width -
-                space
-            );
-
-            // позиция правого бегунка
-            tooltipMaxLeft = Math.min(
-              tooltipMaxHideValueElBounds.left + half,
-              rangeBounds.right - tooltipMinHideValueElBounds.width
-            );
+          // У правого края
+          else if (rangeBounds.right <= tooltipMaxElBounds.right) {
+            tooltipMinShowValueEl.style = [
+              'position: fixed',
+              `top: ${tooltipMinHideValueElBounds.top}px`,
+              `left: ${tooltipMaxShowValueElBounds.left -
+                tooltipMinShowValueElBounds.width -
+                this.space}px`,
+            ].join(';');
           }
 
-          // Сдвигаем левый тултип
-          tooltipMinShowValueEl.style = [
-            'position: fixed',
-            `top: ${tooltipMinHideValueElBounds.top}px`,
-            `left: ${tooltipMinLeft}px`,
-          ].join(';');
+          // В центре
+          else {
+            const diff =
+              tooltipMinHideValueElBounds.right +
+              this.space -
+              tooltipMaxHideValueElBounds.left;
 
-          // Сдвигаем правый тутлип
-          tooltipMaxShowValueEl.style = [
-            'position: fixed',
-            `top: ${tooltipMaxHideValueElBounds.top}px`,
-            `left: ${tooltipMaxLeft}px`,
-          ].join(';');
+            const half = diff / 2;
+
+            const minFixed =
+              tooltipMinHideValueElBounds.left - half < rangeBounds.left;
+
+            const maxFixed =
+              tooltipMaxHideValueElBounds.right + half > rangeBounds.right;
+
+            let tooltipMinLeft = tooltipMinHideValueElBounds.left - half;
+            let tooltipMaxLeft = tooltipMaxHideValueElBounds.left + half;
+
+            // Ближе к левому краю
+            if (minFixed) {
+              // позиция левого бегунка
+              tooltipMinLeft = Math.max(
+                tooltipMinHideValueElBounds.left - half,
+                rangeBounds.left
+              );
+
+              // позиция правого бегунка
+              tooltipMaxLeft = Math.max(
+                tooltipMaxHideValueElBounds.left + half,
+                tooltipMinShowValueElBounds.right + this.space
+              );
+            }
+
+            // Ближе к правому краю
+            if (maxFixed) {
+              // позиция левого бегунка
+              tooltipMinLeft = Math.min(
+                tooltipMinHideValueElBounds.left - half,
+                rangeBounds.right -
+                  tooltipMinHideValueElBounds.width -
+                  tooltipMaxHideValueElBounds.width -
+                  this.space
+              );
+
+              // позиция правого бегунка
+              tooltipMaxLeft = Math.min(
+                tooltipMaxHideValueElBounds.left + half,
+                rangeBounds.right - tooltipMinHideValueElBounds.width
+              );
+            }
+
+            // Сдвигаем левый тултип
+            tooltipMinShowValueEl.style = [
+              'position: fixed',
+              `top: ${tooltipMinHideValueElBounds.top}px`,
+              `left: ${tooltipMinLeft}px`,
+            ].join(';');
+
+            // Сдвигаем правый тутлип
+            tooltipMaxShowValueEl.style = [
+              'position: fixed',
+              `top: ${tooltipMaxHideValueElBounds.top}px`,
+              `left: ${tooltipMaxLeft}px`,
+            ].join(';');
+          }
         }
       }
     }
+
+    return (
+      <Tooltip
+        id={isMin ? this.tooltipIdMin : this.tooltipIdMax}
+        prefixCls="rc-slider-tooltip"
+        overlay={
+          <>
+            <div
+              className="rc-slider-tooltip-value rc-slider-tooltip-hide-value"
+              style={{ visibility: 'hidden' }}
+            >
+              {value}
+            </div>
+            <div
+              className="rc-slider-tooltip-value rc-slider-tooltip-show-value"
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+              }}
+            >
+              {value}
+            </div>
+          </>
+        }
+        visible={true}
+        placement="bottom"
+        key={index}
+        onPopupAlign={this.onPopupAlign}
+      >
+        <Handle value={value} {...restProps} />
+      </Tooltip>
+    );
   }
 
-  return (
-    <Tooltip
-      id={isMin ? tooltipIdMin : tooltipIdMax}
-      prefixCls="rc-slider-tooltip"
-      overlay={
-        <>
-          <div
-            className="rc-slider-tooltip-value rc-slider-tooltip-hide-value"
-            style={{ visibility: 'hidden' }}
-          >
-            {value}
-          </div>
-          <div
-            className="rc-slider-tooltip-value rc-slider-tooltip-show-value"
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-            }}
-          >
-            {value}
-          </div>
-        </>
-      }
-      visible={true}
-      placement="bottom"
-      key={index}
-      onPopupAlign={onPopupAlign}
-    >
-      <Handle value={value} {...restProps} />
-    </Tooltip>
-  );
-};
+  // Получаем доступ к DOM элементам
+  onPopupAlign() {
+    const { tooltipMinEl, tooltipMaxEl } = this.state;
 
-const App = () => {
-  rangeEl = useRef(null);
+    if (!tooltipMinEl && !tooltipMaxEl) {
+      this.setState({
+        tooltipMinEl: document.getElementById(this.tooltipIdMin),
+        tooltipMaxEl: document.getElementById(this.tooltipIdMax),
+      });
+    }
+  }
 
-  return (
-    <Range
-      ref={rangeEl}
-      min={900000}
-      max={19000000}
-      defaultValue={[900000, 19000000]}
-      handle={CustomHandle}
-      step={10000}
-    />
-  );
-};
+  render() {
+    return (
+      <Range
+        ref={this.rangeEl}
+        min={900000}
+        max={19000000}
+        defaultValue={[900000, 19000000]}
+        handle={this.customHandle}
+        step={10000}
+      />
+    );
+  }
+}
 
 export default hot(App);
