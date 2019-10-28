ed26f2411e63e166bc3a1fc8a44595e905580ece
Dmitry Karyakin <skycrazyk@gmail.com>
2019-10-28 11:44:32 +0500

    add wrong slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,23 @@ import Range from './Range';
 class App extends Component {
   render() {
     return (
-      <Range
-        min={900000}
-        max={19000000}
-        defaultValue={[900000, 19000000]}
-        step={10000}
-        space={10}
-      />
+      <>
+        <Range
+          min={2014}
+          max={2019}
+          defaultValue={[2014, 2019]}
+          step={1}
+          space={0}
+          style={{ marginBottom: 100 }}
+        />
+        <Range
+          min={900000}
+          max={19000000}
+          defaultValue={[900000, 19000000]}
+          step={10000}
+          space={10}
+        />
+      </>
     );
   }
 }
